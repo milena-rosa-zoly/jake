e6a3713965d1d93861ae32a3d47d27663a8baee1
Milena Boselli Rosa <milena.rosa@zoly.com.br>
2020-07-23 17:28:58 -0300

    modificado todos os codigos de css de inline para um bloco unico

--- scripts.js
+++ scripts.js
@@ -94,23 +94,15 @@ var script = {
 
     var headerContainer = document.createElement("div");
     section.appendChild(headerContainer);
-    headerContainer.style.display = "flex";
-    headerContainer.style.alignItems = "center";
-    headerContainer.style.justifyContent = "space-between";
-    headerContainer.style.marginBottom = "36px";
+    headerContainer.classList.add("main-content-header-container");
 
     var title = document.createElement("h3");
     headerContainer.appendChild(title);
     title.innerHTML = this.modalSections[sectionIndex].title;
-    title.style.fontSize = "20px";
-
     if (this.modalSections[sectionIndex].cookiePolicyAlways) {
       var span = document.createElement("span");
       headerContainer.appendChild(span);
       span.innerText = "Sempre ativos";
-      span.style.fontSize = "20px";
-      span.style.fontFamily = "Roboto, sans-serif";
-      span.style.color = "#211451";
     }
 
     if (
@@ -125,7 +117,6 @@ var script = {
 
       checkbox.addEventListener("click", (event) => {
         this.modalSections[sectionIndex].cookiePolicy = event.target.checked;
-        console.log(this.modalSections);
       });
     }
 
@@ -199,121 +190,69 @@ var script = {
     generalContainer.id = "general-container";
     document.body.appendChild(generalContainer);
 
-    var cookiesPrivacyDiv = document.createElement("div");
-    generalContainer.appendChild(cookiesPrivacyDiv);
-    cookiesPrivacyDiv.id = "cookies-privacy";
-    style.sheet.insertRule(
-      `#cookies-privacy { align-items: center; background: #f2f2f2; display: flex; font-family: Roboto, sans-serif; padding: 0 16px; position: fixed; z-index: 10000; right: 0; bottom: 0; left: 0; }`
-    );
-    style.sheet.insertRule(`#cookies-privacy h1 { margin-bottom: 8px; }`);
-    style.sheet.insertRule(`#cookies-privacy p { max-width: 1100px; }`);
+    // bottom bar container
+    var cookiesPrivacyContainer = document.createElement("div");
+    generalContainer.appendChild(cookiesPrivacyContainer);
+    cookiesPrivacyContainer.id = "cookies-privacy";
 
-    var cookiesPrivacyContent = document.createElement("div");
-    cookiesPrivacyDiv.appendChild(cookiesPrivacyContent);
-    cookiesPrivacyContent.style.width = "75%";
+    // texts container
+    var cookiesPrivacyTextsContainer = document.createElement("div");
+    cookiesPrivacyTextsContainer.id = "texts-container";
+    cookiesPrivacyContainer.appendChild(cookiesPrivacyTextsContainer);
 
     var cookiesPrivacyTitle = document.createElement("h1");
-    cookiesPrivacyContent.appendChild(cookiesPrivacyTitle);
-    cookiesPrivacyTitle.style.marginBottom = "8px";
+    cookiesPrivacyTextsContainer.appendChild(cookiesPrivacyTitle);
     cookiesPrivacyTitle.innerHTML = "Cookies e Privacidade";
 
     var cookiesPrivacyParagraph = document.createElement("p");
-    cookiesPrivacyContent.appendChild(cookiesPrivacyParagraph);
-    cookiesPrivacyParagraph.style.maxWidth = "1100px";
+    cookiesPrivacyTextsContainer.appendChild(cookiesPrivacyParagraph);
     cookiesPrivacyParagraph.innerHTML = `Com o auxílio dos cookies podemos te conhecer melhor e, assim, recomendar produtos e serviços que sejam do seu interesse. Para saber mais sobre cookies e avaliar nossa Política de Privacidade, é só selecionar suas preferências em Configurar Privacidade.`;
 
-    // !DONE Aqui seria bom deixar algo como 25% da tela para o container dos botões.
+    // buttons container
     var cookiesPrivacyButtonsContainer = document.createElement("div");
     cookiesPrivacyButtonsContainer.id = "buttons-container";
-    cookiesPrivacyDiv.appendChild(cookiesPrivacyButtonsContainer);
-    cookiesPrivacyButtonsContainer.style.width = "25%";
-    cookiesPrivacyButtonsContainer.style.display = "flex";
-    style.sheet.insertRule(
-      "@media screen and (max-width: 1200px) { #buttons-container { flex-direction: column; } }"
-    );
-
-    // cookiesPrivacyButtonsContainer.style.width = "600px";
+    cookiesPrivacyContainer.appendChild(cookiesPrivacyButtonsContainer);
 
     var cookiesPrivacyBtnConfig = document.createElement("button");
-    cookiesPrivacyBtnConfig.classList.add("btn-light");
     cookiesPrivacyButtonsContainer.appendChild(cookiesPrivacyBtnConfig);
     cookiesPrivacyBtnConfig.innerText = "Configurar privacidade";
-    style.sheet.insertRule(
-      ".btn-light { align-items: center; background: #fff; border: 1px solid #211451; border-radius: 4px; color: #211451; cursor: pointer; display: flex; height: 40px; justify-content: center; margin: 10px; padding: 16px; transition: background 0.2s; width: 200px; }"
-    );
-    style.sheet.insertRule(
-      ".btn-light:hover { background: rgba(33, 20, 81, 0.2); }"
-    );
+    cookiesPrivacyBtnConfig.classList.add("btn-light");
 
     var cookiesPrivacyBtnAcceptAll = document.createElement("button");
-    cookiesPrivacyBtnAcceptAll.classList.add("btn-dark");
     cookiesPrivacyButtonsContainer.appendChild(cookiesPrivacyBtnAcceptAll);
     cookiesPrivacyBtnAcceptAll.innerText = "Aceitar todos os cookies";
-    style.sheet.insertRule(
-      ".btn-dark { align-items: center; background: #211451; border: 1px solid #fff; border-radius: 4px; color: #fff; cursor: pointer; display: flex; height: 40px; justify-content: center; margin: 10px; padding: 16px; transition: background 0.2s; width: 200px; }"
-    );
-    style.sheet.insertRule(
-      ".btn-dark:hover { background: rgba(33, 20, 81, 0.8); }"
-    );
+    cookiesPrivacyBtnAcceptAll.classList.add("btn-dark");
 
     // ---------------------
     // --- *** modal *** ---
     // ---------------------
-    style.sheet.insertRule(
-      "#config-privacy * { box-sizing: border-box; margin: 0; outline: 0; padding: 0;}"
-    );
     var modal = document.createElement("div");
     document.body.appendChild(modal);
-    modal.id = "config-privacy";
+    modal.id = "config-privacy-modal";
     modal.classList.add("modal");
-    style.sheet.insertRule(
-      "#config-privacy .modal { background-color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0.4); display: none; height: 100%; left: 0; overflow: auto; position: fixed; top: 0; width: 100%; z-index: 1; }"
-    );
 
     var modalInner = document.createElement("div");
     modalInner.classList.add("modal-inner");
     modal.appendChild(modalInner);
 
-    style.sheet.insertRule(
-      "#config-privacy .modal-inner { background: rgba(255, 255, 255, 0.8); border: 1px solid #888; border-radius: 4px; box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0), 0 7px 14px 0 rgba(50, 50, 93, 0.1); position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); width: 960px; max-width: 80%; height: 580px; max-height: 60%; }"
-    );
-
     var modalTitle = document.createElement("h1");
     modalInner.appendChild(modalTitle);
     modalTitle.innerHTML = "Sua Privacidade";
-    style.sheet.insertRule(
-      "#config-privacy .modal-inner h1 { align-items: center; border-bottom: 1px solid #ccc; color: #333; display: flex; font-family: Roboto, sans-serif; font-size: 32px; height: 80px; justify-content: center;padding: 20px 0; text-align: center; width: 100%; }"
-    );
 
-    var modalContent = document.createElement("div");
-    modalContent.id = "modal-content";
-    modalInner.appendChild(modalContent);
-    style.sheet.insertRule(
-      "#config-privacy #modal-content { display: flex; flex-direction: row; font-family: Roboto, sans-serif; height: calc(100% - 160px); overflow: auto; }"
-    );
+    var mainSection = document.createElement("div");
+    mainSection.id = "main-section";
+    modalInner.appendChild(mainSection);
 
     var menu = document.createElement("nav");
-    modalContent.appendChild(menu);
-    menu.style.display = "flex";
-    menu.style.flexDirection = "column";
-    menu.style.maxWidth = "240px";
-    style.sheet.insertRule(
-      "#config-privacy .menu-item { background: transparent; border: 0; border-left: 5px solid transparent; border-bottom: 1px solid #ccc; cursor: pointer; font-family: Roboto, sans-serif; font-size: 16px; height: 50px; font-weight: bold; padding: 10px 20px; text-align: left; transition: background 0.5s; width: 100%; }"
-    );
-    style.sheet.insertRule(
-      "#config-privacy .menu-item:hover { background: rgba(33, 20, 81, 0.2); }"
-    );
-    style.sheet.insertRule(
-      "#config-privacy .menu-item.menu-item-active { border-left: 5px solid #211451; }"
-    );
+    mainSection.appendChild(menu);
 
     var btnSuaPrivacidade = document.createElement("button");
     menu.appendChild(btnSuaPrivacidade);
     btnSuaPrivacidade.id = "btn--sua-privacidade";
     btnSuaPrivacidade.type = "button";
+    btnSuaPrivacidade.innerText = "Sua Privacidade";
     btnSuaPrivacidade.classList.add("menu-item");
     btnSuaPrivacidade.classList.add("menu-item-active");
-    btnSuaPrivacidade.innerText = "Sua Privacidade";
     btnSuaPrivacidade.addEventListener("click", (event) => {
       this.activateSection("Sua Privacidade");
       btnSuaPrivacidade.classList.add("menu-item-active");
@@ -384,19 +323,21 @@ var script = {
     });
 
     var mainContent = document.createElement("main");
-    modalContent.appendChild(mainContent);
+    mainSection.appendChild(mainContent);
     mainContent.id = "main-content";
-    style.sheet.insertRule(
-      "#config-privacy #modal-content main { border-left: 1px solid #ccc; flex: 1; padding: 24px; }"
-    );
 
     var initialSectionContent = this.modalSections[0];
 
+    var mainSection = document.createElement("section");
+    mainContent.appendChild(mainSection);
+
+    var headerContainer = document.createElement("div");
+    mainSection.appendChild(headerContainer);
+    headerContainer.classList.add("main-content-header-container");
+
     var mainContentTitle = document.createElement("h3");
-    mainContent.appendChild(mainContentTitle);
-    mainContentTitle.innerText = initialSectionContent.title;
-    mainContentTitle.style.fontSize = "20px";
-    mainContentTitle.style.marginBottom = "36px";
+    headerContainer.appendChild(mainContentTitle);
+    mainContentTitle.innerHTML = initialSectionContent.title;
 
     var mainContentParagraph = document.createElement("p");
     mainContent.appendChild(mainContentParagraph);
@@ -404,9 +345,6 @@ var script = {
 
     var footer = document.createElement("footer");
     modalInner.appendChild(footer);
-    style.sheet.insertRule(
-      "#config-privacy .modal-inner footer { align-items: center; border-top: 1px solid #ccc; display: flex; height: 80px; justify-content: center; }"
-    );
 
     var btnSaveConfig = document.createElement("button");
     footer.appendChild(btnSaveConfig);
@@ -418,30 +356,29 @@ var script = {
     footer.appendChild(btnAcceptAll);
     btnAcceptAll.innerText = "Aceitar todos os cookies";
     btnAcceptAll.classList.add("btn-dark");
-    btnAcceptAll.style.marginLeft = "32px";
 
     // ! actions
     cookiesPrivacyBtnConfig.onclick = function () {
       modal.style.display = "block";
     };
 
-    // bar button
+    // bottom bar button
     cookiesPrivacyBtnAcceptAll.onclick = () => {
       this.acceptAllCookies();
-      cookiesPrivacyDiv.style.display = "none";
+      cookiesPrivacyContainer.style.display = "none";
       modal.style.display = "none";
     };
 
     // modal button
     btnAcceptAll.onclick = () => {
       this.acceptAllCookies();
-      cookiesPrivacyDiv.style.display = "none";
+      cookiesPrivacyContainer.style.display = "none";
       modal.style.display = "none";
     };
 
     btnSaveConfig.onclick = () => {
       this.updateCookiesPolicies();
-      cookiesPrivacyDiv.style.display = "none";
+      cookiesPrivacyContainer.style.display = "none";
       modal.style.display = "none";
     };
 
@@ -462,7 +399,195 @@ var script = {
         outline: none;
       }
 
-      .custom-checkbox {
+      #general-container {
+        font-family: Arial, sans-serif;
+      }
+      
+      #cookies-privacy {
+        align-items: center;
+        background: #f2f2f2;
+        bottom: 0;
+        display: flex;
+        font-family: Roboto, sans-serif;
+        left: 0;
+        padding: 0 16px;
+        position: fixed;
+        right: 0;
+        z-index: 10000;
+      }
+      
+      #cookies-privacy #texts-container {
+        width: 75%;
+      }
+      
+      #cookies-privacy #texts-container h1 {
+        font-size: 32px;
+        font-weight: 700;
+        margin-bottom: 8px;
+      }
+      
+      #cookies-privacy #texts-container p {
+        max-width: 1100px;
+      }
+      
+      #cookies-privacy #buttons-container {
+        display: flex;
+        width: 25%;
+      }
+      
+      #cookies-privacy .btn-light {
+        align-items: center;
+        background: #fff;
+        border: 1px solid #211451;
+        border-radius: 4px;
+        color: #211451;
+        cursor: pointer;
+        display: flex;
+        font-size: 14px;
+        height: 40px;
+        justify-content: center;
+        margin: 10px;
+        padding: 16px;
+        transition: background 0.2s;
+        width: 200px;
+      }
+      #cookies-privacy .btn-light:hover {
+        background: rgba(33, 20, 81, 0.2);
+      }
+      
+      #cookies-privacy .btn-dark,
+      #config-privacy-modal .modal-inner footer .btn-dark {
+        align-items: center;
+        background: #211451;
+        border: 1px solid #fff;
+        border-radius: 4px;
+        color: #fff;
+        cursor: pointer;
+        display: flex;
+        font-size: 14px;
+        height: 40px;
+        justify-content: center;
+        margin: 10px;
+        padding: 16px;
+        transition: background 0.2s;
+        width: 200px;
+      }
+      #cookies-privacy .btn-dark:hover,
+      #config-privacy-modal .modal-inner footer .btn-dark:hover {
+        background: rgba(33, 20, 81, 0.8);
+      }
+      
+      #config-privacy-modal * {
+        box-sizing: border-box;
+        margin: 0;
+        outline: 0;
+        padding: 0;
+      }
+      
+      #config-privacy-modal .modal {
+        background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.4);
+        bottom: 0;
+        display: none;
+        height: 100%;
+        left: 0;
+        overflow: auto;
+        position: fixed;
+        right: 0;
+        top: 0;
+        z-index: 1;
+      }
+      
+      #config-privacy-modal .modal-inner {
+        background: rgba(255, 255, 255);
+        border: 1px solid #888;
+        border-radius: 4px;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0), 0 7px 14px 0 rgba(50, 50, 93, 0.1);
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 960px;
+        max-width: 80%;
+        height: 580px;
+        max-height: 60%;
+      }
+      
+      #config-privacy-modal .modal-inner h1 {
+        align-items: center;
+        border-bottom: 1px solid #ccc;
+        color: #333;
+        display: flex;
+        font-family: Arial, sans-serif;
+        font-size: 32px;
+        font-weight: 700;
+        height: 80px;
+        justify-content: center;
+        padding: 20px 0;
+        text-align: center;
+        width: 100%;
+      }
+      
+      #main-section {
+        display: flex;
+        flex-direction: row;
+        font-family: Roboto, sans-serif;
+        height: calc(100% - 160px);
+        overflow: auto;
+      }
+      
+      #main-section nav {
+        display: flex;
+        flex-direction: column;
+        max-width: 260px;
+      }
+      
+      #main-section nav .menu-item {
+        background: transparent;
+        border: 0;
+        border-left: 5px solid transparent;
+        border-bottom: 1px solid #ccc;
+        cursor: pointer;
+        font-family: Roboto, sans-serif;
+        font-size: 16px;
+        font-weight: bold;
+        height: 50px;
+        padding: 10px 20px;
+        text-align: left;
+        transition: background 0.3s;
+        width: 100%;
+      }
+      #main-section nav .menu-item:hover {
+        background: rgba(33, 20, 81, 0.2);
+      }
+      #main-section nav .menu-item.menu-item-active {
+        border-left: 5px solid #211451;
+      }
+      
+      #main-section main {
+        border-left: 1px solid #ccc;
+        flex: 1;
+        padding: 24px;
+      }
+      
+      #main-content main h3 {
+        font-size: 20px;
+        font-weight: 700;
+      }
+      
+      #main-content .main-content-header-container {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 36px;
+      }
+      
+      #main-content .main-content-header-container span {
+        color: #211451;
+        font-size: 20px;
+      }
+      
+      #main-content .main-content-header-container .custom-checkbox {
         -webkit-appearance: none;
         -moz-appearance: none;
         appearance: none;
@@ -478,11 +603,11 @@ var script = {
         width: 3.5em;
       }
       
-      .custom-checkbox:checked {
+      #main-content .main-content-header-container .custom-checkbox:checked {
         background: #211451;
       }
       
-      .custom-checkbox:after {
+      #main-content .main-content-header-container .custom-checkbox:after {
         background: #fff;
         border-radius: 50%;
         -webkit-box-shadow: 0 0 0.25em rgba(0, 0, 0, 0.3);
@@ -498,9 +623,24 @@ var script = {
         width: 1.5em;
       }
       
-      .custom-checkbox:checked:after {
+      #main-content .main-content-header-container .custom-checkbox:checked:after {
         left: calc(100% - 1.5em);
       }
+      
+      #config-privacy-modal .modal-inner footer {
+        align-items: center;
+        border-top: 1px solid #ccc;
+        display: flex;
+        height: 80px;
+        justify-content: center;
+      }
+      
+      @media screen and (max-width: 1200px) {
+        #buttons-container {
+          flex-direction: column;
+        }
+      }
+      
     `;
     document.head.appendChild(style);
 
